91868cd852afc60c9d6ebe6e81a2612300572b9b
efatkhedr99 <efatkhedr1982@gmail.com>
2023-11-18 01:06:43 +0200

    Added laravel-R1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ExampleController;
+use App\Http\Controllers\AddcarController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,46 +18,80 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('user/{name}/{age?}', function ($name,$age=0) {
-    $msg='the username is :'.$name;
-    if($age >0) {
-         $msg .= ' and age is :' .$age;
-    }
-   return $msg;
+// Route::get('user/{name}/{age?}', function ($name,$age=0) {
+//     $msg='the username is :'.$name;
+//     if($age >0) {
+//          $msg .= ' and age is :' .$age;
+//     }
+//    return $msg;
 
 
-   // return 'the username is :'.$name . ' and age is :' .$age;
-})-> whereIn('name',['Efat','MOHAMED']);
+//    // return 'the username is :'.$name . ' and age is :' .$age;
+// })-> whereIn('name',['Efat','MOHAMED']);
 
-Route::get('about', function () {
-    return ('ABOUT Bage');
+// Route::get('about', function () {
+//     return ('ABOUT Bage');
+// });
+// Route::get('contacts', function () {
+//     return ('contacts us page');
+// });
+// Route::prefix('support')->group(function () {
+//     Route::get('chat', function(){
+//     return 'Chat page';
+//     });
+//     Route::get('call', function(){
+//     return 'call page';
+//     });
+//     Route::get('/ticket/{id?}', function ($id=0) {
+//         return 'The id is: ' . $id;
+//         })->where(['id'=> '[0-9]+']);
+//     });
+//     Route::prefix('training')->group(function () {
+//         Route::get('hr', function(){
+//         return 'HR page';
+//         });
+//         Route::get('markting', function(){
+//         return 'Marcting page';
+//         });
+//         Route::get('ict', function () {
+//             return 'ict page ';
+//             });
+//             Route::get('Logistics', function () {
+//                 return 'Logistics page ';
+//                 });
+//         });
+
+
+
+        // Route::fallback(function() {
+        //     return redirect('/');
+        //     });
+
+        Route::get('cv', function () {
+            return view('cv');
+        });
+        Route::get('login', function () {
+            return view('login');
+        });
+        Route::post('receive', function () {
+            return ('DATA receive');
+        })->name('receive');
+
+        Route::get('test1', [ExampleController::class,
+'test1']);
+
+Route::get('addcar', function () {
+    return view('addcar');
 });
-Route::get('contacts', function () {
-    return ('contacts us page');
-});
-Route::prefix('support')->group(function () {
-    Route::get('chat', function(){
-    return 'Chat page';
-    });
-    Route::get('call', function(){
-    return 'call page';
-    });
-    Route::get('/ticket/{id?}', function ($id=0) {
-        return 'The id is: ' . $id;
-        })->where(['id'=> '[0-9]+']);
-    });
-    Route::prefix('training')->group(function () {
-        Route::get('hr', function(){
-        return 'HR page';
-        });
-        Route::get('markting', function(){
-        return 'Marcting page';
-        });
-        Route::get('ict', function () {
-            return 'ict page ';
-            });
-            Route::get('Logistics', function () {
-                return 'Logistics page ';
-                });
-        });
+Route::post('receive1', function () {
+    return ('DATA receive');
+})->name('receive1');
+
+
+Route::get('/post/create', [AddcarController::class,
+'create']);
+Route::post('/post/create', [AddcarController::class,
+'store']);
+
+
 
